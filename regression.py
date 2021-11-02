0e891ab46225ec99d51d1fe5e6875ae424366b3c
dm1737 <davidmartin1737@gmail.com>
2021-11-02 08:52:03 -0400

    metrics set up

--- regression.py
+++ regression.py
@@ -1,15 +1,29 @@
 import pandas as pd
-import quandl 
+import quandl
+import math 
 
 # Get google daily stock data
 df = quandl.get('WIKI/GOOGL')
 
 # Create dataframe with target data
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+
 # Create dataframes with target metrics
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-#Create dataframe which contains targeted metrics
+
+# Create dataframe which contains targeted metrics
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
-print(df.head())
+# Column to predict future values
+forecast_col = 'Adj. Close'
+# To avoid losing data with holes, replace NaN holes with -9999 to force outlier, but retain rest of data.
+df.fillna(-9999, inplace=True)
+
+# Attempt to forecast 1% out from data
+forecast_out = int(math.ceil(0.01*len(df)))
+
+# Shift label column by -forecast percent, causing label column data in row to be that value into future
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.tail())
